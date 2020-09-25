189b2dea5efeceddee555d95967cba349d5d5350
Vladislav Arsenev <forjavatests8@gmail.com>
2020-09-25 21:53:32 +0500

    optimized import

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -3,9 +3,10 @@ package ru.javawebinar.topjava.util;
 import ru.javawebinar.topjava.model.UserMeal;
 import ru.javawebinar.topjava.model.UserMealWithExcess;
 
-import java.time.*;
-import java.time.temporal.ChronoField;
-import java.time.temporal.TemporalField;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Month;
 import java.util.*;
 import java.util.stream.Collectors;
 
