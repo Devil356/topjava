4076d4a8b92baa0306eda61d2d56629d3ce746ee
Vladislav Arsenev <forjavatests8@gmail.com>
2020-10-27 15:29:44 +0500

    Implement JpaMealRepository
    Added NamedQueries to Meal, implements JpaMealRepository.
    It's a first commit of implementing JpaMealRepo

--- src/main/java/ru/javawebinar/topjava/repository/jpa/JpaMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/jpa/JpaMealRepository.java
@@ -2,12 +2,14 @@ package ru.javawebinar.topjava.repository.jpa;
 
 import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.MealRepository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Objects;
 
 @Repository
 public class JpaMealRepository implements MealRepository {
@@ -17,25 +19,45 @@ public class JpaMealRepository implements MealRepository {
 
     @Override
     public Meal save(Meal meal, int userId) {
+        Objects.requireNonNull(meal, "meal must not be null");
+        User ref = em.getReference(User.class, userId);
         if (meal.isNew()) {
+            meal.setUser(ref);
+            em.persist(meal);
+            return meal;
+        } else {
+            User mealUser = meal.getUser();
+            if (mealUser.getId().equals(userId)) {
+                em.merge(meal);
+                return meal;
+            } else {
+                return null;
+            }
+        }
+    }
 
+    @Override
+    public boolean delete(int id, int userId) {
+        return em.createNamedQuery(Meal.DELETE)
+                .setParameter("id", id)
+                .setParameter("userId", userId)
+                .executeUpdate() != 0;
+    }
+
+    @Override
+    public Meal get(int id, int userId) {
+        Meal meal = em.find(Meal.class, id);
+        if (meal.getUser().getId().equals(userId)) {
+            return meal;
         }
         return null;
     }
 
     @Override
-    public boolean delete(int id, int userId) {
-        return false;
-    }
-
-    @Override
-    public Meal get(int id, int userId) {
-        return null;
-    }
-
-    @Override
     public List<Meal> getAll(int userId) {
-        return null;
+        return em.createNamedQuery(Meal.GET_ALL, Meal.class)
+                .setParameter("userId", userId)
+                .getResultList();
     }
 
     @Override
