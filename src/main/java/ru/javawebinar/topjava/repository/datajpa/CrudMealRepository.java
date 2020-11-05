3241780ea88657b41f21dbd39e745364283c5cfa
Vladislav Arsenev <forjavatests8@gmail.com>
2020-11-05 16:28:19 +0500

    Create tests for each profile

--- src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
@@ -3,18 +3,26 @@ package ru.javawebinar.topjava.repository.datajpa;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.model.User;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Transactional(readOnly = true)
 public interface CrudMealRepository extends JpaRepository<Meal, Integer> {
+
     Meal getMealByIdAndUser_Id(int id, int userId);
 
     List<Meal> findAllByUser_IdOrderByIdDesc(int userId);
 
-    @Query(value = "FROM Meal m WHERE m.dateTime >= :startTime AND m.dateTime < :endTime ORDER BY m.id DESC ")
-    List<Meal> getAllBetweenDates(@Param("startTime") LocalDateTime startTime, @Param("endTime") LocalDateTime endTime);
+    @Query(value = "FROM Meal m WHERE m.user.id = :userId " +
+            "AND m.dateTime >= :startTime AND m.dateTime < :endTime ORDER BY m.id DESC ")
+    List<Meal> getAllBetweenDates(
+            @Param("startTime") LocalDateTime startTime,
+            @Param("endTime") LocalDateTime endTime,
+            @Param("userId") int userId
+    );
 
 }
--- src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepository.java
@@ -1,6 +1,7 @@
 package ru.javawebinar.topjava.repository.datajpa;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.MealRepository;
@@ -21,12 +22,15 @@ public class DataJpaMealRepository implements MealRepository {
         this.crudUserRepository = crudUserRepository;
     }
 
+
     @Override
+    @Transactional
     public Meal save(Meal meal, int userId) {
-        User user = crudUserRepository.getOne(userId);
-        meal.setUser(user);
-        crudRepository.save(meal);
-        return meal;
+        if (!meal.isNew() && get(meal.getId(), userId) == null) {
+            return null;
+        }
+        meal.setUser(crudUserRepository.getOne(userId));
+        return crudRepository.save(meal);
     }
 
     @Override
@@ -52,6 +56,6 @@ public class DataJpaMealRepository implements MealRepository {
     @Override
     public List<Meal> getBetweenHalfOpen(LocalDateTime startDateTime, LocalDateTime endDateTime, int userId) {
 
-        return crudRepository.getAllBetweenDates(startDateTime, endDateTime);
+        return crudRepository.getAllBetweenDates(startDateTime, endDateTime, userId);
     }
 }
