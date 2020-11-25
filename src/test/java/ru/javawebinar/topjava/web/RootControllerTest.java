c180c7913d377f56e8299d32019b406c9f5d070f
Vladislav Arsenev <forjavatests8@gmail.com>
2020-11-25 13:57:40 +0500

    Implement testMeals() method in RootControllerTest

--- src/test/java/ru/javawebinar/topjava/web/RootControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/RootControllerTest.java
@@ -9,7 +9,10 @@ import java.util.List;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static ru.javawebinar.topjava.MealTestData.meals;
 import static ru.javawebinar.topjava.UserTestData.*;
+import static ru.javawebinar.topjava.util.MealsUtil.DEFAULT_CALORIES_PER_DAY;
+import static ru.javawebinar.topjava.util.MealsUtil.getTos;
 
 class RootControllerTest extends AbstractControllerTest {
 
@@ -27,6 +30,18 @@ class RootControllerTest extends AbstractControllerTest {
                                 USER_MATCHER.assertMatch(actual, admin, user);
                             }
                         }
+                ));
+    }
+
+    @Test
+    void testMeals() throws Exception {
+        perform(get("/meals"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("meals"))
+                .andExpect(forwardedUrl("/WEB-INF/jsp/meals.jsp"))
+                .andExpect(model().attribute("meals",
+                        getTos(meals, DEFAULT_CALORIES_PER_DAY)
                 ));
     }
 }
